13e746329181399fbc8ae43bedb8d1f3ecb68d4c
Quentin M <mattson.quentin@gmail.com>
2022-08-08 13:52:29 -0700

    Added Key class with fields.

---
+++ src/main/java/com/monsters/view/Keys.java
@@ -0,0 +1,22 @@
+package com.monsters.view;
+
+public abstract class Keys {
+
+  public static final String CHARACTER_STATUS = "character_status";
+  public static final String GAME_SUMMARY = "game_summary";
+  public static final String NEXT_MOVE = "next_move";
+  public static final String WIN_ROUND = "win_round";
+  public static final String WIN_GAME = "win_game";
+  public static final String LOST_GAME = "lost_game";
+  public static final String ROUND_START = "round_start";
+  //public static final String PLAY_STATE
+  public static final String ATTACK_REPORT = "attack_report";
+  public static final String MOVE_PROMPT = "move_prompt";
+  public static final String MONSTER_NAME = "monster_name";
+  public static final String PLAYER_NAME = "player_name";
+  public static final String ILLEGAL_MOVE = "illegal_move";
+  public static final String PLAY_AGAIN = "play_again";
+  public static final String NEGATIVE_RESPONSE = "negative_response";
+
+
+}
