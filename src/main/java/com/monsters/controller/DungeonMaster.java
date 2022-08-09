f8d3ec10e846e8ceea47e6f0a2f96ee2f2451d3c
aaronw9989 <williamsa493@strose.edu>
2022-08-09 15:24:44 -0500

    Added fields, constructor, and setters/getters to DungeonMaster class.

--- src/main/java/com/monsters/controller/DungeonMaster.java
+++ src/main/java/com/monsters/controller/DungeonMaster.java
@@ -1,23 +1,68 @@
 package com.monsters.controller;
 
+
+import com.monsters.model.Character;
+import com.monsters.model.Game;
+import com.monsters.model.Monster;
+import java.io.Reader;
+
 public class DungeonMaster {
-  //Fields
-    //Buffer reader - to read stuff from console
-    //Character name
-    //Monster
-    //Game class
 
+  Reader reader;
+  Monster monster;
+  Character character;
+  Game game;
 
+  public DungeonMaster(Reader reader, Monster monster, Character character, Game game) {
+    this.reader = reader;
+    this.monster = monster;
+    this.character = character;
+    this.game = game;
+  }
 
   //Method for battle : ()
-    //
+  //
 
   //Method for gameOver: ()
-    //
+  //
 
   //Method for gameCompleted: ()
-    //
+  //
 
   //Method for gameWon: ()
-    //
+  //
+
+
+  public Reader getReader() {
+    return reader;
+  }
+
+  public void setReader(Reader reader) {
+    this.reader = reader;
+  }
+
+  public Monster getMonster() {
+    return monster;
+  }
+
+  public void setMonster(Monster monster) {
+    this.monster = monster;
+  }
+
+  public Character getCharacter() {
+    return character;
+  }
+
+  public void setCharacter(Character character) {
+    this.character = character;
+  }
+
+  public Game getGame() {
+    return game;
+  }
+
+  public void setGame(Game game) {
+    this.game = game;
+  }
+
 }
