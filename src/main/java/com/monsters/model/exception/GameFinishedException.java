95e89e6be84d3943de6aba338e0c79b6c86f0350
LashayC <cokleylashay@gmail.com>
2022-08-09 12:33:30 -0400

    Added code for exception classes.

--- src/main/java/com/monsters/model/exception/GameFinishedException.java
+++ src/main/java/com/monsters/model/exception/GameFinishedException.java
@@ -1,5 +1,18 @@
 package com.monsters.model.exception;
 
-public class GameFinishedException {
+public class GameFinishedException extends IllegalStateException {
+
+  //NOTE this looks like overloading. May only need final constructor for Game class.
+  public GameFinishedException(String message){
+    super(message);
+  }
+
+  public GameFinishedException(Throwable cause){
+    super(cause);
+  }
+
+  public GameFinishedException(String message, Throwable cause){
+    super(message, cause);
+  }
 
 }
