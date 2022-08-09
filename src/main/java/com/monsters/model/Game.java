97971b694bff3e53c96f353765ce2e4ce0c183ca
aaronw9989 <williamsa493@strose.edu>
2022-08-09 16:42:10 -0500

    Updated battle monster method, and attack method.

--- src/main/java/com/monsters/model/Game.java
+++ src/main/java/com/monsters/model/Game.java
@@ -8,56 +8,59 @@ public class Game {
   public static final int ATTACK_MOVE = 1;
   private boolean firstMove;
   public State state;
-
   public Monster monster;
+  private Character character;
 
-public Game(State initialState, Character character, Monster monster){
-  this.firstMove = true;
-  state = initialState;
-  this.monster = monster;
-
+  public Game(State initialState, Character character, Monster monster) {
+    this.firstMove = true;
+    state = initialState;
+    this.character = character;
+    this.monster = monster;
   }
 
 
-
-
-
-  public enum State{
-    PLAYER_MOVE{
+  public enum State {
+    PLAYER_MOVE {
       @Override
-      public boolean isAtStart(){
+      public boolean isAtStart() {
         return true;
       }
+
       @Override
-      public boolean isFinished(){
+      public boolean isFinished() {
         return false;
       }
+
       @Override
-      public State nextMoveState(){
+      public State nextMoveState() {
         return MONSTER_MOVE;
       }
+
       @Override
-      public State nextWinState(){
+      public State nextWinState() {
         return MONSTER_WIN;
       }
 
     },
 
-    MONSTER_MOVE{
+    MONSTER_MOVE {
       @Override
-      public boolean isAtStart(){
+      public boolean isAtStart() {
         return true;
       }
+
       @Override
-      public boolean isFinished(){
+      public boolean isFinished() {
         return false;
       }
+
       @Override
-      public State nextMoveState(){
+      public State nextMoveState() {
         return PLAYER_MOVE;
       }
+
       @Override
-      public State nextWinState(){
+      public State nextWinState() {
         return PLAYER_WIN;
       }
 
@@ -69,24 +72,25 @@ public Game(State initialState, Character character, Monster monster){
     private static final String NO_MOVES_ALLOWED_FORMAT = "Game is already in a finished state (HP = %d); no further moves possible";
 
 
-    public boolean isAtStart(){
+    public boolean isAtStart() {
       return false;
     }
 
-    public boolean isFinished(){
+    public boolean isFinished() {
       return false;
     }
 
-    public State nextMoveState(){
+    public State nextMoveState() {
       return null;
     }
 
-    public State nextWinState(){
+    public State nextWinState() {
       return this;
     }
 
-    public State play(int currentMonsterHP, int currentPlayerHP, int attackPower, int numberOfKeysPlayerHolds){
-    return this.nextMoveState();
+    public State play(int currentMonsterHP, int currentPlayerHP, int attackPower,
+        int numberOfKeysPlayerHolds) {
+      return this.nextMoveState();
     }
 
   }
