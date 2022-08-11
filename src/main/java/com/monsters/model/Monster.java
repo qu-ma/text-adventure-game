569a3afcaf0efc0e4a4804a68c86ff0213c4f931
LashayC <cokleylashay@gmail.com>
2022-08-11 10:49:05 -0400

    Removed unused attack method in CharacterModel and Overrides.

--- src/main/java/com/monsters/model/Monster.java
+++ src/main/java/com/monsters/model/Monster.java
@@ -7,13 +7,6 @@ public class Monster extends CharacterModel {
   }
 
   @Override
-  public int attack(int damage) {
-    // TODO add rng for random damage
-    System.out.println("Attack!");
-    return 1;
-  }
-
-  @Override
   public void speak() {
     System.out.println("Roar!");
 
